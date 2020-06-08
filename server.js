0fcef78b8bf0a225aee0507ff9d9187da7c7b327
Marta <marta.anthony1@gmail.com>
2020-06-07 19:09:40 -0700

    debugging

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 'use strict'
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 require('dotenv').config();
 
 app.use(express.static('./public'));
 
 const PORT = process.env.PORT;
- 
-// app.get('/', function (req, res) {
-//   res.send('Hello Dude')
-// })
  
 app.listen(PORT, () => {
   console.log('listening on ${PORT}')
